4a600bf51e51bca42209c9877404cba4cb77fc9f
Wez Young <oldwezyoung@gmail.com>
2021-02-08 13:00:19 +0000

    Added popup page folder, added popup add questionnaire page.

--- TDQ/MainPage.xaml.cs
+++ TDQ/MainPage.xaml.cs
@@ -15,14 +15,9 @@ namespace TDQ
             InitializeComponent();
         }
 
-        private void AddQuestionnaire_Clicked(object sender, EventArgs e)
+        private async void BtnAddQuestionnaire_Clicked(object sender, EventArgs e)
         {
-            CreateQuestionnaireLayout.IsVisible = true;
-        }
-
-        private void btnSendQuestionnaire_Clicked(object sender, EventArgs e)
-        {
-            CreateQuestionnaireLayout.IsVisible = false;
+            await Navigation.PushModalAsync(new PopupPages.AddQuestionnairePage());
         }
     }
 }
