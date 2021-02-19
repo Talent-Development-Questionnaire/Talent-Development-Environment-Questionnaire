6733955b525701a40f61c6fa90824f634ffeebc9
RoryMcKenzie <rorymckenzie@mailbox.org>
2021-02-19 19:48:30 +0000

    Added picker to choose associated group

--- TDQ/PopupPages/AddGoalsGroupPage.xaml.cs
+++ TDQ/PopupPages/AddGoalsGroupPage.xaml.cs
@@ -1,20 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using TDQ.Models;
+
 
 namespace TDQ.PopupPages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddGoalsGroupPage : ContentPage
     {
+        ObservableCollection<Group> Groups;
+        List<string> groupList = new List<string>();
+        string[] emailList;
+
         public AddGoalsGroupPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //Initialising variables
+            Groups = new ObservableCollection<Group>();
+
+            //Gets all the saved files for created groups
+            var files = Directory.EnumerateFiles(App.FolderPath, "*.group.txt");
+            //iterates through each file and adds the Group to the list view
+            foreach (var filename in files)
+            {
+                string text = (File.ReadAllText(filename));
+                string[] splitText = text.Split('\n');
+                emailList = Classes.GroupPageFunctions.PopulateListOnAppearing(splitText);//Method to populate array with emails from the file
+
+                //Adds Group object to list, sets each property of the object
+                Groups.Add(new Group
+                {
+                    Filename = filename,
+                    Name = splitText[0],
+                    GroupNo = Convert.ToInt32(splitText[1]),
+                    ImageFilePath = splitText[2],
+                    EmailList = emailList
+                });
+                groupList.Add(splitText[0]);
+
+                PickerGroup.ItemsSource = groupList.ToArray();
+            }
+
         }
     }
 }
