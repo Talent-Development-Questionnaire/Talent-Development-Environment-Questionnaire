2b55a29a73c7eaf9949399ec2d07043be5d3d65a
Calum Reid <calumreid1@hotmail.co.uk>
2021-04-26 17:26:13 +0100

    Current working __init__.py file live on server.

--- __init__.py
+++ __init__.py
@@ -2,28 +2,37 @@
 import flask
 from flask_mysqldb import MySQL
 from flask import Flask
-from .coach.routes import coach_blueprint
-from .player.routes import player_blueprint
-from .config import app_config
-from .extensions import mysql
+from coach.routes import coach_blueprint
+from player.routes import player_blueprint
+from config import app_config
+from app.extensions import mysql, mail
 
 def create_app(config_name):
 
-	app = flask.Flask(__name__, instance_relative_config=False)
-	app.config.from_object(app_config[config_name])
-	app.config.from_pyfile('config.py')
+        app = flask.Flask(__name__, instance_relative_config=False)
 
-	app.config['MYSQL_HOST'] = 'localhost'
-	app.config['MYSQL_USER'] = 'root'
-	app.config['MYSQL_PASSWORD'] = 'Planetoftheapes12'
-	app.config['MYSQL_DB'] = 'talent_development_questionnaire'
-	
-	mysql.init_app(app)
+        app.config['MYSQL_HOST'] = 'localhost'
+        app.config['MYSQL_USER'] = 'root'
+        app.config['MYSQL_PASSWORD'] = 'Planetoftheapes12'
+        app.config['MYSQL_DB'] = 'talent_development_questionnaire'
+        app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-	app.register_blueprint(coach_blueprint)
-	app.register_blueprint(player_blueprint)
+        app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+        app.config['MAIL_PORT'] = 465
+        app.config['MAIL_USERNAME'] = 'tdq.noreply@gmail.com'
+        app.config['MAIL_PASSWORD'] = 'Planetoftheapes12'
+        app.config['MAIL_USE_TLS'] = False
+        app.config['MAIL_USE_SSL'] = True
+
+        mysql.init_app(app)
+        mail.init_app(app)
+
+        app.register_blueprint(coach_blueprint)
+        app.register_blueprint(player_blueprint, url_prefix='/player')
+
+        return app
 
 
-	return app
+
 
 
