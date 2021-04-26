d1dbf7205d39a4756185b32aac911de1f5c438cf
Calum Reid <calumreid1@hotmail.co.uk>
2021-04-26 18:01:13 +0100

    Current working coach/routes.py file live on server.

--- coach/routes.py
+++ coach/routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, Flask, current_app
+from flask import Blueprint, Flask, jsonify
 from flask_mysqldb import MySQL
+from pathlib import Path
 
-from ..extensions import mysql
+from app.extensions import mysql, mail, Message
 
 coach_blueprint = Blueprint('coach_blueprint', __name__)
 
@@ -19,14 +20,34 @@ def checkEmail(email):
         return 'false'
 
 
-@coach_blueprint.route('/coach/addCoach/<email>/<password>/<name>')
-def addcoach(name, email, password):
+@coach_blueprint.route('/coach/addCoach/<email>/<password>/<name>/<otp>')
+def addcoach(name, email, password, otp):
     cursor = mysql.connection.cursor()
-    cursor.execute("INSERT INTO coaches (name, email, password) VALUES ('%s','%s','%s')" % (name, email, password))
+    cursor.execute("INSERT INTO coaches (name, email, password, gender, dob, otp) VALUES ('%s','%s','%s', ' ', ' ', '%s')" % (name, email, password, otp))
     mysql.connection.commit()
     cursor.close()
+    verify_email(email, otp)
     return 'true'
 
+
+def verify_email(email, otp):
+    msg = Message('TDQ Account Verification', sender='tdq.noreply@gmail.com', recipients=['%s' % email])
+    msg.body = "Thank you for making an account with TDQ, the last step is to verify" \
+               " your account please enter the OTP into the app: %s\n" \
+               "Kind regards,\n" \
+               "The TDQ Team" % otp
+    mail.send(msg)
+
+@coach_blueprint.route('/coach/verifyAccount/<email>/<otp>')
+def verify_account(email, otp):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM coaches WHERE email = '%s' AND otp = '%s'" % (email, otp))
+    row_count = cursor.rowcount
+
+    if row_count == 1:
+        return 'true'
+    else:
+        return 'false'
 
 @coach_blueprint.route('/coach/checkAccountExists/<email>/<password>')
 def checkAccountExists(email, password):
@@ -54,32 +75,59 @@ def get_user_details(email):
 @coach_blueprint.route('/coach/editUser/<id>/<email>/<name>/<gender>/<dob>')
 def edit_user_details(id, email, name, gender, dob):
     cursor = mysql.connection.cursor()
-    cursor.execute("UPDATE coaches SET email = '%s', name = '%s', gender = '%s', dob = '%s' WHERE id = '%s'" %
+    cursor.execute("UPDATE coaches SET email = '%s', name = '%s', gender = '%s', dob = '%s' WHERE coach_id = '%s'" %
                    (email, name, gender, dob, id))
     mysql.connection.commit()
     cursor.execute("SELECT * FROM coaches WHERE email = '%s'" % email)
-
     return str(cursor.fetchall())
 
 
-@coach_blueprint.route('/coach/createQuestionnaire/<name>/<qType>/<email>')
-def createQuestionnaire(name, qType, email):
+
+@coach_blueprint.route('/coach/createQuestionnaire/<name>/<qType>/<email>/<list>/<otp>')
+def createQuestionnaire(name, qType, email, list, otp):
     cursor = mysql.connection.cursor()
-    cursor.execute("INSERT INTO questionnaire (name, type, coach_id) VALUES ('%s','%s', (SELECT coach_id FROM coaches WHERE email = '%s'))" % (name, qType, email))
+
+    cursor.execute("SELECT * FROM questionnaire WHERE name = '%s' AND coach_id = (SELECT coach_id FROM coaches WHERE email = '%s')" % (name, email))
+
+    if cursor.rowcount > 0:
+     addAthletes(list, email, otp, name)
+    else:
+        cursor.execute("INSERT INTO questionnaire (name, type, coach_id) VALUES ('%s','%s', (SELECT coach_id FROM coaches WHERE email = '%s'))" % (name, qType, email))
+        mysql.connection.commit()
+        addAthletes(list, email, otp, name)
+    cursor.execute("SELECT * FROM questionnaire WHERE name = '%s' AND coach_id = (SELECT coach_id FROM coaches WHERE email = '%s')" % (name, email))
+    return str(cursor.fetchall())
+
+
+def addAthletes(athlete, email, otp, quiz_name):
+    cursor =  mysql.connection.cursor()
+    cursor.execute("INSERT INTO emails_verify (questionnaire_id, email, code) VALUES ((SELECT questionnaire_id FROM questionnaire WHERE name = '%s' and coach_id = (SELECT coach_id FROM coaches WHERE email = '%s')), '%s', '%s')" % (quiz_$
     mysql.connection.commit()
-    cursor.close()
+    send_athlete_email(athlete, otp, email)
     return 'true'
+
+def send_athlete_email(athlete, otp, email):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT name FROM coaches WHERE email = '%s'" % email)
+    name = cursor.fetchall()[0]
+    msg = Message('TDQ Coach Questionnaire Invite', sender='tdq.noreply@gmail.com', recipients=['%s' % athlete])
+    msg.body = "Your coach, %s, has invited you to complete a questionnaire on our Talent Development Questionnaire " \
+               "app.\n After downloading the app please go to the questionnaire page and use this OTP: %s\n" \
+               "Kind regards,\n" \
+               "The TDQ Team" % ("".join(name.values()), otp)
+    mail.send(msg)
+
 
 @coach_blueprint.route('/coach/deleteCoach/<email>/<password>')
 def deleteCoach(email, password):
-    if checkAccountExists(email, password) == 'false':
+    if checkAccountExists(email, password) == 'true':
         cursor = mysql.connection.cursor()
         cursor.execute("DELETE FROM coaches WHERE email = '%s'" % email)
         mysql.connection.commit()
         cursor.close()
         return 'true'
-    else:
-        return 'false'
+
+    return 'false'
 
 
 @coach_blueprint.route('/coach/getResults/<qID>/<qNumber>')
@@ -99,6 +147,49 @@ def getResults(qID, qNumber):
     return 'False'
 
 
+@coach_blueprint.route('/coach/getQuestionnaires/<coachID>')
+def get_questionnaires(coachID):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM questionnaire WHERE coach_id = '%s'" % coachID)
+
+    if cursor.rowcount is not 0:
+        return str(cursor.fetchall())
+
+    return 'false'
+
+
+@coach_blueprint.route('/coach/getQuestionnaireQuestions/<qID>')
+def get_questions(qID):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM question WHERE questionnaire_id = '%s'" % qID)
+    if cursor.rowcount is not 0:
+       return str(cursor.fetchall())
+
+    return 'false'
+
+
+@coach_blueprint.route('/coach/getQuestionnaireQuestions/<qID>')
+def get_questions(qID):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM question WHERE questionnaire_id = '%s'" % qID)
+    if cursor.rowcount is not 0:
+       return str(cursor.fetchall())
+
+    return 'false'
+
+
+@coach_blueprint.route('/coach/getQuestions/<qType>')
+def getQuestions(qType):
+
+    path = Path(__file__).parent.absolute()
+    if qType == '59':
+        questions = open("%s/59questions.txt" %(path), "r", encoding="utf-8")
+    else:
+        questions = open("%s/28questions.txt" %(path), "r", encoding="utf-8")
+
+    return questions.read()
+
+
 @coach_blueprint.route('/coach/checkOTP/<email>')
 def checkOTP(email):
     cursor = mysql.connection.cursor()
@@ -106,11 +197,11 @@ def checkOTP(email):
     result = cursor.fetchall()
     otp = result[0]["otp"]
     if otp == 'true':
-        cursor.close()
         return 'True'
-    else:
         cursor.close()
+    else:
         return 'False'
+        cursor.close()
 
 
 @coach_blueprint.route('/coach/verifyOTP/<email>/<otp>')
@@ -129,14 +220,12 @@ def verifyOTP(email, otp):
         return 'False'
 
 
-
-
 @coach_blueprint.route('/coach/recieveAthleteInfo')
 def recieveAthleteInfo():
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM athlete_info")
     result = cursor.fetchall()
-    info = str(result)
+    info  = str(result)
     mysql.connection.commit()
     cursor.close()
     return info
