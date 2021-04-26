3be4a44b26d9ffc9920d1db074033fd7bf2e535d
Calum Reid <calumreid1@hotmail.co.uk>
2021-04-26 18:45:38 +0100

    Current working player/routes.py file live on server.

--- player/routes.py
+++ player/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, Flask, current_app
 from flask_mysqldb import MySQL
-
+from pathlib import Path
 from app.extensions import mysql
 
 player_blueprint = Blueprint('player_blueprint', __name__)
@@ -9,39 +9,41 @@ player_blueprint = Blueprint('player_blueprint', __name__)
 @player_blueprint.route('/getQuestions/<qType>')
 def getQuestions(qType):
 
+    path = Path(__file__).parent.absolute()
     if qType == '59':
-        questions = open("59questions.txt").readlines()
+        questions = open("%s/59questions.txt" %(path), "r", encoding="utf-8")
     else:
-        questions = open("28questions.txt").readlines()
+        questions = open("%s/28questions.txt" %(path), "r", encoding="utf-8")
 
-    return str(questions)
-
+    return questions.read()
 
 
 @player_blueprint.route('/submitQuestion/<qID>/<qNumber>/<qScore>')
 def submitQuestion(qID,qNumber,qScore):
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM question WHERE questionnaire_id = '%s' AND question_no = '%s'" % (qID, qNumber))
-    qExists = cursor.fetchall()
-    if all(qExists) == True:
+
+    if cursor.rowcount is 0:
         cursor.execute("INSERT INTO question (questionnaire_id, question_no, score) VALUES ('%s','%s','%s')" % (qID, qNumber, qScore))
-    else:
-        cursor.execute("SELECT score FROM question WHERE questionnaire_id = '%s' AND question_no = '%s'" % (qID, qNumber))
-        currentScore = cursor.fetchall()
-        newScore = currentScore[0]["score"] + int(qScore)
-        cursor.execute("UPDATE question SET score = '%s' WHERE questionnaire_id = '%s' AND question_no = '%s'" % (newScore, qID, qNumber))
+        mysql.connection.commit()
+        return'True'
+
+    cursor.execute("SELECT score FROM question WHERE questionnaire_id = '%s' AND question_no = '%s'" % (qID, qNumber))
+    currentScore = cursor.fetchall()
+    newScore = currentScore[0]["score"] + int(qScore)
+    cursor.execute("UPDATE question SET score = '%s' WHERE questionnaire_id = '%s' AND question_no = '%s'" % (newScore, qID, qNumber))
 
     mysql.connection.commit()
     cursor.close()
-    updateCount(qID)
     return 'True'
 
-@player_blueprint.route('/updateCount/<qID>')
+
+@player_blueprint.route('/updateCompletionCount/<qID>')
 def updateCount(qID):
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT completions FROM questionnaire WHERE questionnaire_id = '%s'" % qID)
     countExists = cursor.fetchall()
-    if countExists == 0:
+    if cursor.rowcount is 0:
         cursor.execute("UPDATE questionnaire SET completions = 1 WHERE questionnaire_id = '%s'" % qID)
     else:
         newCount = countExists[0]["completions"] + 1
@@ -49,17 +51,23 @@ def updateCount(qID):
 
     mysql.connection.commit()
     cursor.close()
+    return 'true'
+
+@player_blueprint.route('/verifyAthlete/<email>/<otp>')
+def verify_athlete(email, otp):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM questionnaire WHERE questionnaire_id = (SELECT questionnaire_id FROM emails_verify WHERE email = '%s' AND code = '%s')" % (email, otp))
+    if(cursor.rowcount is not 0):
+        return str(cursor.fetchall())
+
+    return 'false'
 
 
-@player_blueprint.route('/player/sendAthleteInfo/<sportAcademy>/<sport>/<name>/<age>/<gender>')
+@player_blueprint.route('/sendAthleteInfo/<sportAcademy>/<sport>/<name>/<age>/<gender>')
 def sendAthleteInfo(sportAcademy, sport, name, age, gender):
     cursor = mysql.connection.cursor()
     cursor.execute("INSERT INTO athlete_info (sport_academy, sport, name, age, gender) VALUES ('%s','%s','%s','%s','%s')" % (sportAcademy, sport, name, age, gender))
     mysql.connection.commit()
     cursor.close()
     return 'true'
-
-
-
-
 
